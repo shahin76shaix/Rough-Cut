56fddbc0bf286a9e93923ef1b01b8a82430b27a0
shahin76shaix <shaix.official@gmail.com>
2023-03-14 02:05:09 +0330

    revision before master

--- app/script.js
+++ app/script.js
@@ -1,15 +1,18 @@
+// Open navigation bar
 document.querySelector(".openNav").addEventListener("click", function () {
   document.querySelector(".aside").style.marginRight = "0";
   document.querySelector(".openNav").style.opacity = "0.2";
   document.querySelector(".aside-background").style.marginLeft = "0";
 });
 
+//Close navigation bar with btn
 document.querySelector(".closeNav").addEventListener("click", function () {
   document.querySelector(".aside").style.marginRight = "-250rem";
   document.querySelector(".openNav").style.opacity = "1";
   document.querySelector(".aside-background").style.marginLeft = "-250rem";
 });
 
+//Close navigation bar with background
 document
   .querySelector(".aside-background")
   .addEventListener("click", function () {
